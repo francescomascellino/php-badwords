80498a4f86d4f9f2c0fd5dee8012ca9b2105aeac
francescomascellino <franesco.mascellino@gmail.com>
2023-10-09 13:32:05 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+    <form action="post">
+        <textarea name="paragraph" id="paragraph" cols="30" rows="10" placeholder="Enter your text here"></textarea>
+    </form>
+</body>
+
+</html>
